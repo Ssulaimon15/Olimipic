7be52d78ae788fd22bb1ef687e1320dbab5cc15d
Sarah Sulaimon <ssula170@d127.org>
2022-06-08 21:28:09 -0500

    Made the front page and the sign in and log in page

---
+++ Olimipic/SignUpViewController.swift
@@ -0,0 +1,88 @@
+//
+//  SignUpViewController.swift
+//  Olimipic
+//
+//  Created by Sarah Sulaimon on 6/6/22.
+//
+
+import UIKit
+
+class SignUpViewController: UIViewController {
+
+    @IBOutlet weak var ageTextField: UITextField!
+    
+    @IBOutlet weak var ageAdder: UIStepper!
+    
+    @IBOutlet weak var passwordTextField: UITextField!
+    
+    @IBOutlet weak var passwordStrength: UILabel!
+    
+    @IBOutlet weak var showHidenLabel: UIButton!
+    
+    @IBOutlet weak var confirmPassword: UITextField!
+    
+    @IBOutlet weak var confirmPasswordButton: UIButton!
+    
+    @IBOutlet weak var visableLabel: UILabel!
+    
+    var passwordVisible: Bool = true
+    
+    var showPassword: Bool = true
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        ageAdder.wraps = true
+        ageAdder.autorepeat = true
+        ageAdder.maximumValue = 100
+        
+        configureView()
+        
+        // Do any additional setup after loading the view.
+    }
+    @IBAction func ageAdderStepper(_ sender: UIStepper) {
+        ageTextField.text = Int(sender.value).description
+    }
+    func configureView(){
+            passwordTextField.isSecureTextEntry = true
+            passwordTextField.clearsOnBeginEditing = false
+            showHidenLabel.setTitle("Show", for: .normal)
+            passwordStrength.isHidden = true
+        }
+    @IBAction func showHide() {
+            if passwordVisible {
+                passwordTextField.isSecureTextEntry = false
+                showHidenLabel.setTitle("Hide", for: .normal)
+                passwordVisible = false
+            } else {
+                passwordTextField.isSecureTextEntry = true
+                showHidenLabel.setTitle("Show", for: .normal)
+                passwordVisible = true
+            }
+        }
+    @IBAction func passwordType(_ sender: UITextField) {
+        
+        passwordStrength.isHidden = false
+               if (passwordTextField.text?.count)! < 3 {
+                   passwordStrength.text = "Password is weak"
+                   passwordStrength.textColor = UIColor.red
+               } else if (passwordTextField.text?.count)! > 8 {
+                   passwordStrength.text = "Password is unbreakable"
+                   passwordStrength.textColor = UIColor.green
+               } else {
+                   passwordStrength.text = "Password is safer"
+                   passwordStrength.textColor = UIColor.purple
+               }
+    }
+    
+   
+}
+
+/*
+// MARK: - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+    // Get the new view controller using segue.destination.
+    // Pass the selected object to the new view controller.
+}
+*/
